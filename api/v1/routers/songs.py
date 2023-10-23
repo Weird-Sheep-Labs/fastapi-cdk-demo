7da4adb09a3d271a8e2045dd49c73402a0661907
Armand Rego <armand@weirdsheeplabs.com>
2023-10-23 21:18:39 +0100

    Add delete songs endpoint.

--- api/v1/routers/songs.py
+++ api/v1/routers/songs.py
@@ -4,8 +4,13 @@ from models import Song
 router = APIRouter()
 
 
+@router.get("/")
+async def root():
+    return {"message": "Welcome to the FastAPI CDK demo by Weird Sheep Labs!"}
+
+
 @router.get("/songs/", tags=["songs"])
-async def get_songs():
+async def list_songs():
     return list(Song.scan())
 
 
@@ -13,3 +18,9 @@ async def get_songs():
 async def create_song(song: Song):
     song.save()
     return song
+
+
+@router.get("/songs/delete/", tags=["songs"])
+async def delete_songs():
+    for song in Song.scan():
+        song.delete()
